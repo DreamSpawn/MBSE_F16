53095147f774363d197342ec05120233f554dfbd
Sandie <sandieAkkermans@hotmail.com>
2016-04-15 13:21:31 +0200

    Nu virker places ægte.

--- main/YAWL-project/src/YAWL_net/StartEndConditions.java
+++ main/YAWL-project/src/YAWL_net/StartEndConditions.java
@@ -1,16 +1,18 @@
 package YAWL_net;
 
+import java.awt.Shape;
 import java.util.Iterator;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.IValidationContext;
+import org.eclipse.gmf.runtime.notation.View;
 import org.pnml.tools.epnk.pnmlcoremodel.PetriNet;
 
 /**
- * This is a constraint saying that a YAWL net should have exactly one start place and one
- * end place.
+ * This is a constraint saying that a YAWL net should have exactly one start
+ * place and one end place.
  *
  * @author ekki@dtu.dk
  * @generated NOT
@@ -29,22 +31,30 @@ public class StartEndConditions extends AbstractModelConstraint {
 				while (iterator.hasNext()) {
 					EObject content = iterator.next();
 
-					if(content instanceof Place){
-						Place place = (Place) content;
-						if(place.getType().equals(PlaceTypes.START)){ startCount++;	}
-						if(place.getType().equals(PlaceTypes.END)){	endCount++;	}	
+					if (content instanceof View) {
+						View view = (View) content;
+						
+						if (view.getElement() instanceof Place) {
+							Place place = (Place) view.getElement();
+							if (place.getType().getText().equals(PlaceTypes.START)) {
+								startCount++;
+							}
+							if (place.getType().getText().equals(PlaceTypes.END)) {
+								endCount++;
+							}
+						}
 					}
 				}
 				if (startCount != 1 || endCount != 1) {
 					// if there is not exactly one start place and exactly one
 					// end place, return a failure status.
-					return ctx.createFailureStatus(new Object[] {container});
-					
+					return ctx.createFailureStatus(new Object[] { container });
+
 				}
-				
+
 			}
 
-		} 
+		}
 		// otherwise return a success status
 		return ctx.createSuccessStatus();
 	}
